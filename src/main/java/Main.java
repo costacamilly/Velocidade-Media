1558011230609d579a14c7ab10ed3f4277d1ba02
Camilly <169068284+costacamilly@users.noreply.github.com>
2024-09-02 12:18:08 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+System.out.println("Programa utilizado para calcular a velocidade em Km/h");
+System.out.println(" ");
+System.out.println("A distância considera é:");
+double distancia = 400;
+System.out.println(distancia);
+System.out.println(" ");
+System.out.println("O tempo gasto foi de:");
+double tempo = 3;
+System.out.println(tempo);
+System.out.println(" ");
+System.out.println("A velocidade Média é de:");
+double velocidadeMedia = distancia/tempo;
+System.out.println(velocidadeMedia);
+  }
+
+
+}
